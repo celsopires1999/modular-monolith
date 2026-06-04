3bdee3bb946e0d505b1f4b6e41a3628529f5a15e
Celso Pires <celso.pires1999@gmail.com>
2026-06-04 17:40:13 -0300

    5.09 - create event handlers and integration events

--- FC4.HotelReservation/modular monolith/modules/Reservations/FC4.HotelReservation.Reservations.Application/EventHandlers/ReservationCanceledEventHandler.cs
+++ FC4.HotelReservation/modular monolith/modules/Reservations/FC4.HotelReservation.Reservations.Application/EventHandlers/ReservationCanceledEventHandler.cs
@@ -1,21 +1,30 @@
 using FC4.HotelReservation.Reservations.Domain.Events;
 using FC4.HotelReservation.Reservations.Domain.Repositories;
+using FC4.HotelReservation.Reservations.Events.IntegrationEvents;
+using MassTransit;
 using MediatR;
 
 namespace FC4.HotelReservation.Reservations.Application.EventHandlers;
 
-public class ReservationCanceledEventHandler(IRoomTypeInventoryRepository roomTypeInventoryRepository)
+public class ReservationCanceledEventHandler(
+    IRoomTypeInventoryRepository roomTypeInventoryRepository,
+    IPublishEndpoint publishEndpoint)
     : INotificationHandler<ReservationCanceledEvent>
 {
     public async Task Handle(ReservationCanceledEvent notification, CancellationToken cancellationToken)
     {
         var inventories = await roomTypeInventoryRepository.GetInventoryForPeriodAsync(
             notification.HotelId, notification.RoomTypeId, notification.StayPeriod, cancellationToken);
-        
+
         foreach (var inventory in inventories)
         {
             inventory.ReleaseRooms(notification.RoomQuantity);
             await roomTypeInventoryRepository.UpdateAsync(inventory, cancellationToken);
         }
+
+        var integrationEvent = new ReservationStatusChanged(
+            notification.ReservationId,
+            notification.Status.ToString());
+        await publishEndpoint.Publish(integrationEvent, cancellationToken);
     }
 }
